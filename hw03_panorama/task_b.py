9490f3ed22b00d7f4322c2283515904b477045ce
Arthur <66648337+Artur4ik2304@users.noreply.github.com>
2025-04-16 18:32:27 +0300

    Update task_b.py

--- hw03_panorama/task_b.py
+++ hw03_panorama/task_b.py
@@ -1,7 +1,35 @@
-import cv2
+import numpy as np
 
-def extract_key_points(img1, img2):
-    sift = cv2.SIFT_create()
-    kpts1, desc1 = sift.detectAndCompute(img1, None)
-    kpts2, desc2 = sift.detectAndCompute(img2, None)
-    return kpts1, desc1, kpts2, desc2
+# do not change the code in the block below
+# __________start of block__________
+class DummyMatch:
+    def __init__(self, queryIdx, trainIdx, distance):
+        self.queryIdx = queryIdx  # index in des1
+        self.trainIdx = trainIdx  # index in des2
+        self.distance = distance
+# __________end of block__________
+
+
+def match_key_points_numpy(des1: np.ndarray, des2: np.ndarray) -> list:
+    """
+    Match descriptors using brute-force matching with cross-check.
+
+    Args:
+        des1 (np.ndarray): Descriptors from image 1, shape (N1, D)
+        des2 (np.ndarray): Descriptors from image 2, shape (N2, D)
+
+    Returns:
+        List[DummyMatch]: Sorted list of mutual best matches.
+    """
+    # YOUR CODE HERE
+    distances = np.zeros((des1.shape[0], des2.shape[0]))
+    for i in range(des1.shape[0]):
+      for j in range(des2.shape[0]):
+        distances[i, j] = np.linalg.norm(des1[i] - des2[j])
+
+    matches = []
+    for i in range(des1.shape[0]):
+      temp_ind = np.argmin(distances[i])
+      if np.argmin(distances[:, temp_ind]) == i:
+        matches.append(DummyMatch(i, temp_ind, distances[i, temp_ind]))
+    return sorted(matches, key=lambda x: x.distance)
